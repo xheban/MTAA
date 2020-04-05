706622854ed140d9e0495e23e28c30350a74ab2b
JoZo <michaljozefek@gmail.com>
2020-04-05 19:57:52 +0200

    uprava skriptov na testovanie

--- create-restaurant.php
+++ create-restaurant.php
@@ -1,78 +1,52 @@
 <?php
+header("Content-Type:application/json");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $entityBody = file_get_contents('php://input');
+    $params = json_decode($entityBody, true);
+    $keys = array("food_id", "name", "delivery", "min_price", "city_id");
+    if(arrayKeysExists($keys,$params)){
 
-if(!(isset($_GET['name']) && $_GET['name']!="")){
-	echo "Missing name!";
-	die();
+        $con = mysqli_connect("localhost", "root", "", "mtaa");
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            die();
+        }
+        $sql = "INSERT INTO restaurants SET name = ?, delivery = ?, min_price = ?, city_id = ?, rating = 0";
+        if ($stmt = $con->prepare($sql)) {
+
+            $stmt->bind_param("sddi", $params['name'],$params['delivery'],$params['min_price'],$params['city_id']);
+            $stmt->execute();
+            if($stmt->affected_rows == 1){
+                response(200,"Restaurant was created! ");
+            }else{
+                response(400,"One of input parameters are not valid");
+            }
+            $stmt->close();
+        }else{
+            response(400,"Unexpected error");
+        }
+    }else{
+        response(400,"One of input parameters are missing");
+    }
+
+
+}else{
+    response(400,"invalid type");
 }
 
-if(!(isset($_GET['delivery']) && $_GET['delivery']!="")){
-	echo "Missing delivery time!";
-	die();
+function arrayKeysExists($keys,$array){
+    foreach ($keys as $key){
+        if(!array_key_exists($key,$array)){
+            return false;
+        }
+    } return true;
 }
 
-if(!(isset($_GET['min_price']) && $_GET['min_price']!="")){
-	echo "Missing minimal price!";
-	die();
+function response($response_code,$response_desc){
+    $response['response_code'] = $response_code;
+    $response['response_desc'] = $response_desc;
+    $json_response = json_encode($response);
+    echo $json_response;
+    exit();
 }
-
-//if(!(isset($_GET['open_from']) && $_GET['open_from']!="")){
-//	echo "Missing Opening time!";
-//	die();
-//}
-
-//if(!(isset($_GET['open_to']) && $_GET['open_to']!="")){
-//	echo "Missing Closing time!";
-//	die();
-//}
-
-if(!(isset($_GET['city_id']) && $_GET['city_id']!="")){
-	echo "Missing city_id";
-	die();
-}
-
-if(!(isset($_GET['rating']) && $_GET['rating']!="")){
-	echo "Missing rating";
-	die();
-}
-
-$user = 'root';
-$pass = '';
-$db = 'mtaa';
-
-$conn = new mysqli('localhost', $user, $pass, $db) or die("Nepripojene!!");
-
-if ($conn->connect_error) {
-    	die("Connection failed: " . $conn->connect_error);
-}
-
-$name = $_GET['name'];
-$delivery = $_GET['delivery'];
-$min_price = $_GET['min_price'];
-//$open_from = $_GET['open_from'];
-//$open_to = $_GET['open_to'];
-$city_id = $_GET['city_id'];
-$rating = $_GET['rating'];
-
-$sql = "SELECT * FROM CITIES WHERE id = $city_id";
-$result = mysqli_query($conn ,$sql);
-
-if (mysqli_num_rows($result)!=0){
-
-	$sql = "INSERT INTO RESTAURANTS (name, min_price, delivery, rating, city_id) VALUES('$name','$min_price','$delivery','$rating','$city_id')";
-
-	if ($conn->query($sql) === TRUE) {
-    		echo "New record created successfully";
-	} 
-	else {
-    		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-}
-
-else{
-	echo "City with this ID dont exist!";
-}
-
-mysqli_close($conn);
-
-?> 
 
--- deleteFoodFromRestaurant.php
+++ deleteFoodFromRestaurant.php
@@ -1,27 +1,28 @@
 <?php
 header("Content-Type:application/json");
 if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    if(isset($_GET['id']) && isset($_GET['restaurant_id'])){
-        $Id = $_GET['id'];
-        $restaurantId = $_GET['restaurant_id'];
-        $con = mysqli_connect("localhost", "root", "", "mtaa");
-        if (mysqli_connect_errno()) {
-            echo "Failed to connect to MySQL: " . mysqli_connect_error();
-            die();
-        }
-        $sql = "DELETE FROM food WHERE id = ? AND restaurant_id = ?";
-        if ($stmt = $con->prepare($sql)) {
-            $stmt->bind_param("ii", $Id,$restaurantId);
-            $stmt->execute();
-            if($stmt->affected_rows > 0){
-                response(200,"Food deleted from cart");
-            }else{
-                response(400,"One of input parameters are not valid");
-            }
-            $stmt->close();
-        }else{
-            response(400,"Unexpected error");
-        }
+    	$entityBody = file_get_contents('php://input');
+    	$params = json_decode($entityBody, true);
+    	$keys = array("id", "restaurant_id");
+	if(arrayKeysExists($keys,$params)){
+        	$con = mysqli_connect("localhost", "root", "", "mtaa");
+        	if (mysqli_connect_errno()) {
+            		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            	die();
+        	}
+        	$sql = "DELETE FROM food WHERE id = ? AND restaurant_id = ?";
+        	if ($stmt = $con->prepare($sql)) {
+            		$stmt->bind_param("ii", $params['id'],$params['restaurant_id']);
+            		$stmt->execute();
+            		if($stmt->affected_rows > 0){
+                		response(200,"Food deleted from restauran!");
+            		}else{
+                		response(400,"One of input parameters are not valid");
+            		}
+            		$stmt->close();
+        	}else{
+            		response(400,"Unexpected error");
+        	}
     }else{
         response(400,"One of input parameters are missing");
     }
@@ -37,4 +38,12 @@ function response($response_code,$response_desc){
     $json_response = json_encode($response);
     echo $json_response;
     exit();
+}
+
+function arrayKeysExists($keys,$array){
+    foreach ($keys as $key){
+        if(!array_key_exists($key,$array)){
+            return false;
+        }
+    } return true;
 }
